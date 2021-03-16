a18d1be7dee61b001b6bbee016c3e2f87cd528c4
aslnstbk <asyl.zed.han@gmail.com>
2021-03-16 12:35:52 +0600

    refactoring presentation

--- app/src/main/java/kz/eztech/stylyts/presentation/activity/MainActivity.kt
+++ app/src/main/java/kz/eztech/stylyts/presentation/activity/MainActivity.kt
@@ -1,25 +1,27 @@
 package kz.eztech.stylyts.presentation.activity
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
 import kotlinx.android.synthetic.main.activity_main.*
 import kz.eztech.stylyts.R
 import kz.eztech.stylyts.presentation.base.BaseActivity
+import kz.eztech.stylyts.presentation.utils.extensions.hide
+import kz.eztech.stylyts.presentation.utils.extensions.show
 
 class MainActivity : BaseActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         bottomNavigationView.setupWithNavController(findNavController(R.id.nav_host_fragment))
     }
 
-    fun hideBottomNavigationView(){
-        bottomNavigationView.visibility = View.GONE
+    fun hideBottomNavigationView() {
+        bottomNavigationView.hide()
     }
-    fun displayBottomNavigationView(){
-        bottomNavigationView.visibility = View.VISIBLE
+
+    fun displayBottomNavigationView() {
+        bottomNavigationView.show()
     }
 }
--- app/src/main/java/kz/eztech/stylyts/presentation/activity/SplashActivity.kt
+++ app/src/main/java/kz/eztech/stylyts/presentation/activity/SplashActivity.kt
@@ -1,7 +1,6 @@
 package kz.eztech.stylyts.presentation.activity
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.animation.Animation
 import android.view.animation.AnimationUtils
@@ -11,26 +10,28 @@ import kz.eztech.stylyts.data.models.SharedConstants
 import kz.eztech.stylyts.presentation.base.BaseActivity
 
 class SplashActivity : BaseActivity() {
-	override fun onCreate(savedInstanceState: Bundle?) {
-		super.onCreate(savedInstanceState)
-		setContentView(R.layout.activity_splash)
-		
-		val animation = AnimationUtils.loadAnimation(this,R.anim.fade_in_scale_in)
-		animation.setAnimationListener(object : Animation.AnimationListener{
-			override fun onAnimationStart(animation: Animation?) {}
-			
-			override fun onAnimationEnd(animation: Animation?) {
-				val currentToken = getSharedPrefByKey<String>(SharedConstants.TOKEN_KEY)
-				if(currentToken.isNullOrEmpty()){
-					startActivity(Intent(this@SplashActivity,AuthorizationActivity::class.java))
-					finish()
-				}else{
-					startActivity(Intent(this@SplashActivity,MainActivity::class.java))
-					finish()
-				}
-			}
-			override fun onAnimationRepeat(animation: Animation?) {}
-		})
-		text_view_activity_splash_title.startAnimation(animation)
-	}
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        val animation = AnimationUtils.loadAnimation(this, R.anim.fade_in_scale_in)
+        animation.setAnimationListener(object : Animation.AnimationListener {
+            override fun onAnimationStart(animation: Animation?) {}
+
+            override fun onAnimationEnd(animation: Animation?) {
+                val currentToken = getSharedPrefByKey<String>(SharedConstants.TOKEN_KEY)
+                if (currentToken.isNullOrEmpty()) {
+                    startActivity(Intent(this@SplashActivity, AuthorizationActivity::class.java))
+                    finish()
+                } else {
+                    startActivity(Intent(this@SplashActivity, MainActivity::class.java))
+                    finish()
+                }
+            }
+
+            override fun onAnimationRepeat(animation: Animation?) {}
+        })
+        text_view_activity_splash_title.startAnimation(animation)
+    }
 }
